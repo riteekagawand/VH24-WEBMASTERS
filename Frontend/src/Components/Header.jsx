fd0fe146f9c00d5af46fbf9ab4e88d3d92451fda
SiddhiHate13 <siddhihaate@gmail.com>
2024-09-28 05:17:19 +0530

    Landing Page corrected

--- Frontend/src/Components/Header.jsx
+++ Frontend/src/Components/Header.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -16,12 +17,11 @@ const Header = () => {
           <div className="text-xl font-bold">
             largerthan<i className="fas fa-heart text-red-500"></i>
           </div>
-          <nav className="space-x-4">
-            <a href="#" className="text-gray-700">Home</a>
-            <a href="#" className="text-gray-700">About us</a>
-            <a href="#" className="text-gray-700">What We Do</a>
-            <a href="#" className="text-gray-700">Media</a>
-            <a href="#" className="text-gray-700">Contact</a>
+          <nav className="space-x-12">
+            <a href="#" className="text-gray-700 font-semibold">
+            Home</a>
+            <a href="#" className="text-gray-700 font-semibold">About us</a>
+            <a href="#" className="text-gray-700 font-semibold">Contact</a>
           </nav>
           <button onClick={handleVolunteerClick} className="bg-yellow-500 text-white px-6 py-2 font-semibold rounded-lg border-2 border-white">
             Login!
--- Frontend/src/Components/HeroSection.jsx
+++ Frontend/src/Components/HeroSection.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import video from '../assets/mainvid.mp4';
 import { useNavigate } from 'react-router-dom';
 
